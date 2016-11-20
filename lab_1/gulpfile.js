f2b3d2e326102b7cc28afcb13bb0e7f119164fe6
Dmitriy <vitkom97@mail.ru>
2016-11-20 22:59:45 +0200

    Update gulpfile.js

--- lab_1/gulpfile.js
+++ lab_1/gulpfile.js
@@ -1,19 +1,19 @@
-var gulp        = require('gulp'), // Подключаем Gulp // Подключаем Sass пакет,
-    browserSync = require('browser-sync'), // Подключаем Browser Sync
-    concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-    uglify      = require('gulp-uglifyjs'); // Подключаем gulp-uglifyjs (для сжатия JS)
+var gulp        = require('gulp'), 
+    browserSync = require('browser-sync'),
+    concat      = require('gulp-concat'), 
+    uglify      = require('gulp-uglifyjs'); 
 
 gulp.task('browser-sync', function(){
 	browserSync({
 		server: {
 			baseDir: 'app'
 		},
-		notify: false //отключаем уведомления
+		notify: false
 	});
 });
 
 gulp.task('watch', ['browser-sync'], function(){
-	gulp.watch('app/**/*.html', browserSync.reload);  //livereload для всех файлов.html находящихся в папках и подпапках
-	gulp.watch('app/js/**/*.js', browserSync.reload); //livereload для всех файлов.js находящихся в папках и подпапках
+	gulp.watch('app/**/*.html', browserSync.reload); 
+	gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
 gulp.task('default', ['watch','browser-sync']);
