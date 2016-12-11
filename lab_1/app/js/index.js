e702fb633e2c05c6d021d9279af387a629d698b0
Kamarouski D.Y <dzkamarouski@gmail.com>
2016-12-11 23:20:59 +0200

    poshlopoehalo

--- lab_1/app/js/index.js
+++ lab_1/app/js/index.js
@@ -1,13 +1,24 @@
+"use strict";
+
 var content;
-var timerId = setInterval( function() {
-  $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      content = JSON.stringify(data);
-      console.log(content);
-      var template = Handlebars.compile( $('#template').html() );
-      $('.results').append(template(data));
-    }
+function poshlopoehalo(){
+  return new Promise((resolve, reject) => {
+     var timerId = setInterval( function() {
+      $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function(data) {
+          content = JSON.stringify(data);
+          console.log(content);
+          var template = Handlebars.compile( $('#template').html() );
+          $('.results').append(template(data));
+        }
+      });
+    },10000);
+  })
+  .catch(error => {
+    alert(error); // Error: Not Found
   });
-},10000);
+};
+
+poshlopoehalo();
