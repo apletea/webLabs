830516ab5bfc67c60a59d5fead49f6b802c3346f
Dzmitry <dzkamarouski@gmail.com>
2017-02-02 14:27:39 +0300

    Update script.ps1

--- lab_2/script.ps1
+++ lab_2/script.ps1
@@ -1,3 +1,2 @@
 foreach ( $item in Get-Content C:\ps-test\group2.txt ) { New-Item -ItemType "directory" -Path "c:\ps-test\group2\$item" }
-foreach ( $item in Get-Content C:\ps-test\group9.txt ) { New-Item -ItemType "directory" -Path "c:\ps-test\group9\$item" }
 #вместо с:\ps-test\укажите ваш путь
